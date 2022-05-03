63d1e2284567a1fbdf6bceaefa7ce5ead9213959
jaimechacon11 <chacon.jaime99g@gmail.com>
2022-05-03 16:57:50 -0500

    Práctica 2 y 3 Week 5

--- server3.js
+++ server3.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const app = express();
+const express=require('express');
+const app=express();
 app.use(express.json());
-const port = process.env.PORT || 3000;
-
-// Require para usar Prisma
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const port=process.env.PORT || 3000;
+const{PrismaClient}=require('@prisma/client');
+const prisma=new PrismaClient();
+  //Cors
+const cors=require("cors");
+const corsOptions={
+    origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.json({message: 'alive'});
